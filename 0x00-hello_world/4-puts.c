7aecb851be1da0b02b9833c51da80f41e296c053
omarnate1 <omarnate@gmail.com>
2022-09-08 22:08:45 +0000

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Servers as the entry point for the program
+ *
+ * Return: By default returns zero if no errors otherwise returns non-zero value.
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle")
+	return (0)
+}
