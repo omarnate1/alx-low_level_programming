4475f496f28609413bb759176091656451623cdd
omarnate1 <omarnate@gmail.com>
2022-09-14 20:14:19 +0000

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * _isalpha - this program checks whether a xtr
+ * is from the English alphabet
+ * @c: input parameter
+ * Return: 1 for English xter
+ * 0 if the chataracter is not Enlgish
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
