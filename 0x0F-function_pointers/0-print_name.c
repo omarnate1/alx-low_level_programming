b6600c5680209d89743f15ededdb036d12683226
omarnate1 <omarnate@gmail.com>
2022-10-12 17:14:30 +0000

    task done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,7 @@
  * @f: function to print name
  * Return: Nothing
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 		f(name);
